f04b1d1413e92700d8db529d91a5235121332bc1
He Jiang win gh <he.jiang@sjsu.edu>
2020-04-30 21:51:59 -0700

    Updates

---
+++ precache-manifest.83184bb8c524c57dde1f304ef359013b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4905313718d9f26964b73827a467e16",
+    "url": "/bus-app/index.html"
+  },
+  {
+    "revision": "571c55fc932d4e5e08a9",
+    "url": "/bus-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "342694789b3f6801d094",
+    "url": "/bus-app/static/js/2.90afc2a1.chunk.js"
+  },
+  {
+    "revision": "2fe2eb2360a44cdec691f979d5c12ef8",
+    "url": "/bus-app/static/js/2.90afc2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571c55fc932d4e5e08a9",
+    "url": "/bus-app/static/js/main.36dc0adb.chunk.js"
+  },
+  {
+    "revision": "7f2c9e83083ad9b05269",
+    "url": "/bus-app/static/js/runtime-main.9f9d5da1.js"
+  },
+  {
+    "revision": "dd3bc86f0bc08c690c7505caf3fc31e5",
+    "url": "/bus-app/static/media/bus.dd3bc86f.svg"
+  }
+]);
